7866f21b945739708d2bf8ba7f202507eda6b676
jackdo69 <jackie.duc.do@gmail.com>
2022-06-05 15:06:12 +1000

    Initial commit

---
+++ test/todo-app.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TodoApp from '../lib/todo-app-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/todo-app-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TodoApp.TodoAppStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
